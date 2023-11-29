fc6da65ba6f5bd402272ab7f31c5eca95003226c
Tiedaman33 <orinabravin6@gmail.com>
2023-11-29 14:15:39 -0800

    current commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * check_cycle - fucntion checks if singly linked lsit has a cycle in it
+ * @list: pointer to th beginning of the node
+ * Return: o if no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current, *check;
+	
+	if (list == NULL || list->next == NULL)
+		return(0);
+	current = list;
+	check = current->next;
+	
+	while (current != NULL && check->next != NULL
+			&& check->next->next != NULL)
+	{
+		if(current == check)
+			return (1);
+		current = current->next;
+		check= check->next->next;
+	}
+	return (0);
+}
--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 number = 3.14157
 print(f"Float: {number:.2f}")
