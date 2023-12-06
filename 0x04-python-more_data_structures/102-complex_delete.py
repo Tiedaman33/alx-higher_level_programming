be919e0d11e7db9137878ec33ab712269baffbd3
Tiedaman33 <orinabravin6@gmail.com>
2023-12-06 10:38:59 -0800

    data structures

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delet(a_dictionary, value):
+    list_keys = list(a_dictionary.keys())
+
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
+
+            return (a_dictionary)
