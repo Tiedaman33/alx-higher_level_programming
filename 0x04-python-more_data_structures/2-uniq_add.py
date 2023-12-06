fc0b55f4ec202c7444527c024ff9eb98493ea90a
Tiedaman33 <orinabravin6@gmail.com>
2023-12-06 10:48:23 -0800

    final

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
     uniq_list = set(my_list)
     num = 0
@@ -6,4 +7,4 @@ def uniq_add(my_list=[]):
     for i in uniq_list:
         num += i
 
-        return (num)
+    return num
