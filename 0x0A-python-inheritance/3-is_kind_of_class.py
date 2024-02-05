19ff41f9e45d452f89112f71b1dbc622b8efebd1
Tiedaman33 <orinabravin6@gmail.com>
2024-02-05 13:30:17 -0800

    inehritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """Defines an inherited class-checking function."""
 
 
-def inherits_from(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """Checks if an object is an inherited instance of a class.
 
     Args:
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -7,4 +7,4 @@ class BaseGeometry:
 
     def area(self):
         """Not implemented."""
-        raise Exception("area() is not impelemented")
+        raise Exception("area() is not implemented")
