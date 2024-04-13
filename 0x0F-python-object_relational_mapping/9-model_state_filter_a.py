18c4f5f4a12df0591849b73df913304434d6d9c1
Tiedaman33 <orinabravin6@gmail.com>
2024-04-13 13:41:15 -0700

    orm

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,42 +1,41 @@
 #!/usr/bin/python3
-"""Script to list all State objects that
-contain the letter 'a' from the database hbtn_0e_6_usa.
 
-Args:
-    username (str): The MySQL username.
-    password (str): The MySQL password.
-    db_name (str): The name of the MySQL database.
-
-Returns:
-    None
+"""
+Script to list all State objects that contain the letter
+'a' from the database hbtn_0e_6_usa.
 
 Usage:
-    Run this script with three
-    arguments: username, password, and database name.
-    For example: python3 script.py username password hbtn_0e_6_usa
+    python script.py <mysql_username> <mysql_password> <database_name>
+
+Arguments:
+    mysql_username: The username to connect to the MySQL server.
+    mysql_password: The password to connect to the MySQL server.
+    database_name: The name of the database to connect to.
 
 Requirements:
     - SQLAlchemy module must be installed.
     - State and Base classes must be imported from model_state module.
-    - The script connects to a MySQL server running on localhost at port 3306.
-    - Results are sorted in ascending order by states.id.
-    - Only State objects containing the letter 'a' are included in the results.
-    - The results are displayed in the format: "<state.id>: <state.name>".
-
-Note:
-    The code is not executed when imported.
 """
 
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
 import sys
-from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    # Create the SQLAlchemy engine using the provided MySQL credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a session factory
     Session = sessionmaker(bind=engine)
+
+    # Create a session object
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        print(instance.id, instance.name, sep=": ")
+
+    # Retrieve the states with letter 'a' from the
+    # database and print its ID and name
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
