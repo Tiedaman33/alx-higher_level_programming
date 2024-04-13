f9e88c2355065ddb9161e4f3c4c4151829ef34dd
Tiedaman33 <orinabravin6@gmail.com>
2024-04-13 07:06:31 -0700

    orm

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -29,11 +29,10 @@ Example:
     (3, 'Arizona')
     ...
 """
-
 import MySQLdb
 import sys
 
-if __nam__ == "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
             passwd=sys.argv[2],  db=sys.argv[3], port=3306)
     c = db.cursor()
