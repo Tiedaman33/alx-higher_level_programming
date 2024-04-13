e67627e890400b1a21d37bf57237436e3c40bb74
Tiedaman33 <orinabravin6@gmail.com>
2024-04-13 10:48:48 -0700

    orm

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
 
-"""a script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa"""
+"""Script to print the State object with the name passed as argument from the database hbtn_0e_6_usa.
 
+Args:
+    username (str): The MySQL username.
+    password (str): The MySQL password.
+    db_name (str): The name of the MySQL database.
+    state_name (str): The name of the state to search for.
+
+Returns:
+    None
+
+Usage:
+    Run this script with four arguments: username, password, database name, and state name to search.
+    For example: python3 script.py username password hbtn_0e_6_usa "New York"
+
+Requirements:
+    - SQLAlchemy module must be installed.
+    - State and Base classes must be imported from model_state module.
+    - The script connects to a MySQL server running on localhost at port 3306.
+    - The state object with the provided name is printed.
+    - If no state has the name searched for, "Not found" is displayed.
+
+Note:
+    The code is not executed when imported.
+"""
 if __name__ == "__main__":
 
     from sqlalchemy import create_engine
@@ -16,10 +39,8 @@ if __name__ == "__main__":
 
     conn_str = "mysql+mysqldb://{}:{}@localhost:3306/{}"
     engine = create_engine(conn_str.format(inp[1], inp[2], inp[3]))
-    Session = sessionmaker(bind=engine)
-
     Base.metadata.create_all(engine)
-
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     my_query = session.query(State).filter(State.name.like(inp[4])).all()
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,29 @@
-#!/usr/bin/python3
-"""script that lists all State objects from the database hbtn_0e_6_usa"""
+#!/usr/bin/pythoon3
+"""Script to list all State objects that contain the letter 'a' from the database hbtn_0e_6_usa.
+
+Args:
+    username (str): The MySQL username.
+    password (str): The MySQL password.
+    db_name (str): The name of the MySQL database.
+
+Returns:
+    None
+
+Usage:
+    Run this script with three arguments: username, password, and database name.
+    For example: python3 script.py username password hbtn_0e_6_usa
+
+Requirements:
+    - SQLAlchemy module must be installed.
+    - State and Base classes must be imported from model_state module.
+    - The script connects to a MySQL server running on localhost at port 3306.
+    - Results are sorted in ascending order by states.id.
+    - Only State objects containing the letter 'a' are included in the results.
+    - The results are displayed in the format: "<state.id>: <state.name>".
+
+Note:
+    The code is not executed when imported.
+"""
 
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
@@ -7,9 +31,10 @@ import sys
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine ('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Session = sessionmaker(bind=engine)
+    engine = create_engine ('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
     for instance in session.query(State).filter(State.name.like('%a%')):
         print(instance.id, instance.name, sep=": ")
