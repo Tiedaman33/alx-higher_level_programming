448c7ddc58119d7a6e4b42f6c71230731585e6e2
Tiedaman33 <orinabravin6@gmail.com>
2024-04-26 05:03:28 -0700

    pynetwrok

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script to post data (url-encoded) to a server
-curl -s -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
+# This script sends a POST request to a URL with parameters and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
