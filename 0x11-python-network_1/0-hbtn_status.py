f08af1e6989dfb783db2496674920b88859e172a
Tiedaman33 <orinabravin6@gmail.com>
2024-04-26 03:21:00 -0700

    python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status and displays the body of the response."""
+"""Fetches https://alx-intranet.hbtn.io/status and
+displays the body of the response."""
 
 import urllib.request
+
 
 def fetch_status():
     """Fetches the status from the given URL."""
@@ -11,10 +13,10 @@ def fetch_status():
         utf8_content = body.decode('utf-8')
     return body, utf8_content
 
+
 if __name__ == "__main__":
     body, utf8_content = fetch_status()
     print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
     print("\t- utf8 content: {}".format(utf8_content))
-
