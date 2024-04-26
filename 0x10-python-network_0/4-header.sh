63c09aa1aea4c51f1fc82697c308842eae0776e5
Tiedaman33 <orinabravin6@gmail.com>
2024-04-26 05:00:06 -0700

    pynetwork

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script to send custom headers to servers
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# This script sends a GET request to a displays the body of the response
+curl -s -H "X-School-User-Id: 98" "$1"
