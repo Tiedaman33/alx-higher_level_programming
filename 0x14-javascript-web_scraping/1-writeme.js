903d8e046583c60f935bf1960af7e0f80a5b903b
Tiedaman33 <orinabravin6@gmail.com>
2024-05-22 12:29:53 -0700

    web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,18 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
-const content = process.argv[3];
 
-if (!filePath || !content) {
-  console.error('Usage: ./writeToFile.js <file path> <string to write>');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, content, 'utf8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
-    console.error('Error writing to the file:', err);
-  } else {
-    console.log('File written successfully.');
+    console.log(err);
   }
 });
